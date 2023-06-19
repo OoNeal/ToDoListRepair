8f60ff4c377d29d8ef439152b098448bc37f940a
Elian ARNAUD <83591301+OoNeal@users.noreply.github.com>
2023-06-19 10:11:28 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'todo_list_app.dart';
+
+void main() {
+  runApp(const ToDoListApp());
+}
---
+++ lib/todo_list_app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ToDoListApp extends StatefulWidget {
+  const ToDoListApp({super.key});
+
+  @override
+  State<ToDoListApp> createState() => _ToDoListAppState();
+}
+
+class _ToDoListAppState extends State<ToDoListApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Elephant'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}

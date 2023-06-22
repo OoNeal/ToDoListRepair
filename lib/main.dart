ef0d32d25c5460ca86837cb3ced25aa382e3a113
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2023-06-22 19:37:27 +0200

    Etape 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'todo_list_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env");
+
+  final supabaseUrl = dotenv.env['SUPABASE_URL']!;
+  final supabaseKey = dotenv.env['SUPABASE_API_KEY']!;
 
   await Supabase.initialize(
-    url: "https://ygoeyfvuuwxtjibqczym.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlnb2V5ZnZ1dXd4dGppYnFjenltIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYyOTIwNTAsImV4cCI6MjAwMTg2ODA1MH0.fdReNCjDGLrgC1et3uWY7IPx-4IOYhEmB0ratmMUHhY",
+    url: supabaseUrl,
+    anonKey: supabaseKey,
   );
   runApp(const ToDoListApp());
 }
